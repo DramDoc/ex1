c4ae95752ffdf99880af5b225a053584f2a0407f
unknown <dima.derkach2004@gmail.com>
2020-11-24 17:39:14 +0500

    Разные цвета

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5 import uic
+from ui import Ui_Form
 from PyQt5.QtGui import QPainter, QColor
 import random
 import sys
 
 
-class App(QWidget):
+class App(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.firstStart = True
 
         self.pushButton.clicked.connect(self.update)
@@ -23,7 +24,7 @@ class App(QWidget):
             qp.end()
 
     def drawEllipse(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randrange(0, 255, 1), random.randrange(0, 255, 1), random.randrange(0, 255, 1)))
         r = random.randrange(100, 500, 1)
         x, y = 400 - int(r / 2), 300 - int(r / 2)
         qp.drawEllipse(x, y, r, r)
